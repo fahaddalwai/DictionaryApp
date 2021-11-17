d651bdd71a191c038c1934a743df7a0b64c22035
dalwa <dalwaifahad@gmail.com>
2021-11-17 22:41:51 +0300

    Added UseCase

--- app/src/main/java/com/example/dictionaryapp/feature/domain/use_case/GetWordInfo.kt
+++ app/src/main/java/com/example/dictionaryapp/feature/domain/use_case/GetWordInfo.kt
@@ -1,4 +1,18 @@
 package com.example.dictionaryapp.feature.domain.use_case
 
-class GetWordInfo {
+import com.example.dictionaryapp.core.util.Resource
+import com.example.dictionaryapp.feature.domain.model.WordInfo
+import com.example.dictionaryapp.feature.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetWordInfo(
+    private val repository:WordInfoRepository
+) {
+    operator fun invoke(word:String): Flow<Resource<List<WordInfo>>>{
+        if(word.isBlank()){
+            return flow{}
+        }
+        return repository.getWordInfo(word)
+    }
 }
